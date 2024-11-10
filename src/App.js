7fb3db0e60ec6e18be509eb268552c02e249078c
mamentudicky <dickymamentu@gmail.com>
2024-11-10 15:15:53 +0800

    add folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Clients from "./components/Clients";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body-wrap">
+      <Header />
+      <Hero />
+      <Clients />
+      <Features />
+      <Pricing />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
